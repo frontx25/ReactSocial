1143682d0cf31ad624fbc63f4e9485d5d6608a5d
front <front25@gmail.com>
2023-01-18 20:56:31 +0300

    update 18/01/23 update 3

--- socialnetwork/src/components/Profile/MyPosts/MyPosts.jsx
+++ socialnetwork/src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,7 +5,15 @@ import s from './MyPosts.module.css'
 const MyPosts = (props) => {
 
 
-    let postsElements = props.postData.map(p => <Post message={p.message} likesCount={p.likesCount} avatar={(p.avatar)}/>)
+    let postsElements = props.postData.map(p => <Post message={p.message} likesCount={p.likesCount}
+                                                      avatar={(p.avatar)}/>)
+
+    let newPostElement = React.createRef()
+
+    let addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    }
 
     return (
         <div className={s.postsBlock}>
@@ -13,10 +21,10 @@ const MyPosts = (props) => {
             <div>New post</div>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
 
